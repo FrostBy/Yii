582a84c6db2949d4c9512d55d23dd109dac81a60
Frost <frost.by@gmail.com>
2013-03-18 15:47:30 +0300

    First

--- protected/models/Post.php
+++ protected/models/Post.php
@@ -24,6 +24,9 @@ class Post extends CActiveRecord
 	 * @param string $className active record class name.
 	 * @return Post the static model class
 	 */
+    const STATUS_DRAFT=1;
+    const STATUS_PUBLISHED=2;
+    const STATUS_ARCHIVED=3;
 	public static function model($className=__CLASS__)
 	{
 		return parent::model($className);
@@ -44,15 +47,17 @@ class Post extends CActiveRecord
 	{
 		// NOTE: you should only define rules for those attributes that
 		// will receive user inputs.
-		return array(
-			array('title, content, status, author_id', 'required'),
-			array('status, create_time, update_time, author_id', 'numerical', 'integerOnly'=>true),
-			array('title', 'length', 'max'=>128),
-			array('tags', 'safe'),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, title, content, tags, status, create_time, update_time, author_id', 'safe', 'on'=>'search'),
-		);
+        return array(
+            array('title, content, status', 'required'),
+            array('title', 'length', 'max'=>128),
+            array('author_id', 'numerical', 'integerOnly'=>true),
+            array('status', 'in', 'range'=>array(1,2,3)),
+            array('tags', 'match', 'pattern'=>'/^[\w\s,]+$/',
+                'message'=>'В тегах можно использовать только буквы.'),
+            array('tags', 'normalizeTags'),
+
+            array('title, status', 'safe', 'on'=>'search'),
+        );
 	}
 
 	/**
@@ -62,10 +67,14 @@ class Post extends CActiveRecord
 	{
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
-		return array(
-			'comments' => array(self::HAS_MANY, 'Comment', 'post_id'),
-			'author' => array(self::BELONGS_TO, 'User', 'author_id'),
-		);
+        return array(
+            'author' => array(self::BELONGS_TO, 'User', 'author_id'),
+            'comments' => array(self::HAS_MANY, 'Comment', 'post_id',
+                'condition'=>'comments.status='.Comment::STATUS_APPROVED,
+                'order'=>'comments.create_time DESC'),
+            'commentCount' => array(self::STAT, 'Comment', 'post_id',
+                'condition'=>'status='.Comment::STATUS_APPROVED),
+        );
 	}
 
 	/**
@@ -109,4 +118,16 @@ class Post extends CActiveRecord
 			'criteria'=>$criteria,
 		));
 	}
+    public function normalizeTags($attribute,$params)
+    {
+        $this->tags=Tag::array2string(array_unique(Tag::string2array($this->tags)));
+    }
+    public function getUrl()
+    {
+
+        return Yii::app()->createUrl('post/view', array(
+            'id'=>$this->id,
+            'title'=>$this->title,
+        ));
+    }
 }
